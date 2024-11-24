2c74fbfb35f977ba1d9502be118841bc7665ac55
DunjaNesic <ndunja2001@gmail.com>
2024-11-24 18:01:29 +0100

    added decision tree for diagnosis probability

--- src/tools_methods_project/symptom_checker.clj
+++ src/tools_methods_project/symptom_checker.clj
@@ -3,18 +3,54 @@
 
 ;;until I integrate with AI
 (def symptom-disease-map
-  {"fever" ["Flu" "COVID-19"]
-   "cough" ["Bronchitis" "COVID-19"]
-   "headache" ["Migraine" "Tension Headache"]
-   "chest pain" ["Heart Attack" "GERD"]})
+  {"fever" ["Flu" "COVID-19" "Malaria" "Dengue"]
+   "cough" ["Bronchitis" "COVID-19" "Pneumonia" "Tuberculosis"]
+   "headache" ["Migraine" "Tension Headache" "Cluster Headache" "Sinusitis" "Brain cancer"]
+   "chest pain" ["Heart Attack" "GERD" "Angina" "Panic Attack"]
+   "fatigue" ["Anemia" "Hypothyroidism" "Chronic Fatigue Syndrome" "Depression"]
+   "sore throat" ["Strep Throat" "Flu" "COVID-19" "Tonsillitis"]
+   "shortness of breath" ["Asthma" "COPD" "COVID-19" "Pneumonia"]
+   "rash" ["Allergy" "Chickenpox" "Measles" "Eczema"]
+   "abdominal pain" ["Appendicitis" "IBS" "Gastritis" "Gallstones"]
+   "dizziness" ["Vertigo" "Low Blood Pressure" "Dehydration" "Anemia"]})
+
 
 (def disease-specialist-map
   {"Flu" "General Practitioner"
    "COVID-19" "Infectious Disease Specialist"
-   "Bronchitis" "Pulmologist"
+   "Malaria" "Infectious Disease Specialist"
+   "Dengue" "Infectious Disease Specialist"
+   "Bronchitis" "Pulmonologist"
+   "Pneumonia" "Pulmonologist"
+   "Tuberculosis" "Pulmonologist"
    "Migraine" "Neurologist"
+   "Brain cancer" "Neurologist"
+   "Tension Headache" "General Practitioner"
+   "Cluster Headache" "Neurologist"
+   "Sinusitis" "ENT Specialist"
    "Heart Attack" "Cardiologist"
-   "Tummy Ache" "Gastroenterologist"})
+   "GERD" "Gastroenterologist"
+   "Angina" "Cardiologist"
+   "Panic Attack" "Psychiatrist"
+   "Anemia" "Hematologist"
+   "Hypothyroidism" "Endocrinologist"
+   "Chronic Fatigue Syndrome" "General Practitioner"
+   "Depression" "Psychiatrist"
+   "Strep Throat" "ENT Specialist"
+   "Tonsillitis" "ENT Specialist"
+   "Asthma" "Pulmonologist"
+   "COPD" "Pulmonologist"
+   "Allergy" "Allergist"
+   "Chickenpox" "Dermatologist"
+   "Measles" "Infectious Disease Specialist"
+   "Eczema" "Dermatologist"
+   "Appendicitis" "Surgeon"
+   "IBS" "Gastroenterologist"
+   "Gastritis" "Gastroenterologist"
+   "Gallstones" "Gastroenterologist"
+   "Vertigo" "Neurologist"
+   "Low Blood Pressure" "Cardiologist"
+   "Dehydration" "General Practitioner"})
 
 ;;(get map key default-value)
 (defn predict-diagnoses
