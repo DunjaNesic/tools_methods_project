d4d38c0c65cd9761c3a2471f2f698f4dcda7fb7c
DunjaNesic <ndunja2001@gmail.com>
2024-12-27 01:24:36 +0100

    started using HoneySQL instead of raw SQL

--- src/tools_methods_project/user.clj
+++ src/tools_methods_project/user.clj
@@ -1,17 +1,20 @@
 (ns tools-methods-project.user
   (:require [next.jdbc :as jdbc]
             [tools-methods-project.db :as db]
-            [buddy.hashers :as hashers]))
+            [buddy.hashers :as hashers]
+            [honey.sql :as sql]))
 
 (defn get-all-patients []
-  (let [query "SELECT * FROM PATIENT"]
-    (jdbc/execute! db/datasource [query])))
+  (let [query (sql/format {:select [:*] :from [:PATIENT]})]
+    (jdbc/execute! db/datasource query)))
 
 (defn register-patient [email password]
   (let [hashed-password (hashers/derive password)
-        query "INSERT INTO PATIENT (email, password) VALUES (?, ?)"]
+        query (sql/format {:insert-into :PATIENT
+                           :columns [:email :password]
+                           :values [[email hashed-password]]})]
     (try
-      (jdbc/execute! db/datasource [query email hashed-password])
+      (jdbc/execute! db/datasource query)
       {:status :success, :message "Patient registered successfully"}
       (catch Exception e
         {:status :error, :message (.getMessage e)}))))
@@ -19,8 +22,10 @@
 (def sessions (atom {}))
 
 (defn login-patient [email password]
-  (let [query "SELECT * FROM PATIENT WHERE email = ?"
-        result (jdbc/execute! db/datasource [query email])]
+  (let [query (sql/format {:select [:*]
+                           :from [:PATIENT]
+                           :where [:= :email email]})
+        result (jdbc/execute! db/datasource query)]
     (if-let [patient (first result)]
       (let [stored-password (:patient/password patient)]
         (if (hashers/check password stored-password)
@@ -33,5 +38,3 @@
 (defn logout-patient [email]
   (swap! sessions dissoc email)
   {:status :success, :message "Logged out successfully"})
-
-;; sqlite npr, neka embedded baza koja ce da popuni podatke itd
