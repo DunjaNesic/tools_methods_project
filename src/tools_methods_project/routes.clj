72b745bc46bd4a26b8e1d4de54435b4d8d109643
DunjaNesic <ndunja2001@gmail.com>
2025-01-03 16:08:55 +0100

    fixed CORS problem

--- src/tools_methods_project/routes.clj
+++ src/tools_methods_project/routes.clj
@@ -1,10 +1,24 @@
 (ns tools-methods-project.routes
   (:require
    [ring.adapter.jetty :refer [run-jetty]]
+   [ring.util.response :refer [header response]]
    [tools-methods-project.handlers.personalized-treatment-handler :refer [personalized-treatment]]
    [tools-methods-project.handlers.symptom-checker-handler :refer [check-symptoms-handler]]
    [tools-methods-project.handlers.group-chat-handler :refer [group-chat-handler]]
    [tools-methods-project.handlers.specialist-chat-handler :refer [specialist-chat-handler]]))
+
+(defn cors-response [response]
+  (-> response
+      (header "Access-Control-Allow-Origin" "http://localhost:8280")
+      (header "Access-Control-Allow-Methods" "GET, POST, OPTIONS")
+      (header "Access-Control-Allow-Headers" "Content-Type, Authorization")
+      (header "Access-Control-Allow-Credentials" "true")))
+
+(defn cors-middleware [handler]
+  (fn [request]
+    (if (= (:request-method request) :options)
+      (cors-response (response ""))
+      (cors-response (handler request)))))
 
 (defn app [request]
   (case (:uri request)
@@ -25,4 +39,4 @@
 
 (defn -main []
   (println "Starting server on http://localhost:3000")
-  (run-jetty app {:port 3000 :join? false}))
+  (run-jetty (cors-middleware app) {:port 3000 :join? false})) 
